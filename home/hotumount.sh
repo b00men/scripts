a6191d19e3b2247d9f1b1bec78aaac1443bf1998
Nikita <b00men.pochta@gmail.com>
2016-07-09 13:31:08 +0700

    Undefine changes.

--- home/hotumount.sh
+++ home/hotumount.sh
@@ -1,27 +1,17 @@
 #!/bin/sh
-
-# Полностью отмонтировать устройство /dev/sdb,
-# остановить диск, послать сигнал на отключение устройства,
-# вывестии сообщение о разрешении на вынимание диска.
-# При ошибке на любом из этапов прекратить работу и
-# вывести ошибку.
-
-# todo:
-# аргументы, notify, интеграция
-
-mntstr=`mount | grep sdb | sed 's_\(/dev/sdb.\) \(.*\)_\1_'`
+mntstr=`mount | grep sdb | awk '{print $3}' | tac`
 
 if [ -b /dev/sdb ] ; then
 	if [ -n "$mntstr" ] ; then
 		for i in "$mntstr"
 		do
-			sudo umount $i || exit
+			sudo umount -R $i || exit
 		done
 	fi
 	sync
 	sudo scsi_stop /dev/sdb || exit
 	sudo sh -c " echo 1 > /sys/block/sdb/device/delete" || exit
-	echo "Complete prepare for hot umount /dev/sdb,\n	now device can be unplug."
+	echo "Complete prepare for hot umount /dev/sdb,\n	now device can be ubplug."
 else
 	echo "/dev/sdb don't exist."
 fi
